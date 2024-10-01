c6a06d1f1fb70bdcff8f7af7fc0840dec5024ae8
snipe82 <pcoronadognet@gmail.com>
2024-10-01 14:27:25 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/app.js',
+        '/images/icon-192x192.png',
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
